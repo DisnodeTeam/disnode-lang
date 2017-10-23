728d734d604d9541f15871ec9bd56cfe064f0e7c
FireGamer3 <gm31334@gmail.com>
2017-10-23 00:19:04 -0500

    bugfixes

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
 const logger = require("disnode-logger");
 class Lang {
-  constructor() {
+  constructor(dir) {
+    this.dir = dir;
     logger.Success("Lang","Start","New Lang instance created!")
   }
   getPack(lang, pack){
     try {
       logger.Info("Lang","getPack","Attempt to get pack: " + lang + "/" + pack);
-      return require("./lang/" + lang + "/" + pack);
+      return require(this.dir + "/lang/" + lang + "/" + pack);
     } catch (e) {
       logger.Error("Lang","getPack","Error when getting pack: " + lang + "/" + pack + "  ::  " + e);
     }
   }
   getSupportedLanguages(cb){
-    fs.readdir("./lang", function(err, files) {
+    var self = this;
+    var FileDir = self.dir;
+    fs.readdir(FileDir + "/lang", function(err, files) {
       var dirs = [];
       for (var index = 0; index < files.length; ++index) {
         var file = files[index];
         if (file[0] !== '.') {
-          var filePath = "./lang/" + file;
+          var filePath = FileDir + "/lang/" + file;
           fs.stat(filePath, function(err, stat) {
             if (stat.isDirectory()) {
               dirs.push(this.file);
